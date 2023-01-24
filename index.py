0d9a8f6dd02685936f2bc793d980ea702784e38f
rony derra <derrarony@gmail.com>
2023-01-24 19:12:22 +0200

    print

--- index.py
+++ index.py
@@ -1,7 +1,16 @@
-from web3 import Web3, EthereumTesterProvider
-print(Web3.toHex(0))
-w3 = Web3(EthereumTesterProvider())
-print(w3.isConnected())
-print(w3.eth.chain_id)
-# x = w3.eth.get_transaction_receipt("0x95df96a2d57ae58aa624d2d384264b4ec4e1930a0c3ea0c282e0f771694d54d9")
-# print(x)
+from web3 import Web3
+import codecs
+
+print("Strating Script")
+my_provider = Web3.HTTPProvider('https://polygon-rpc.com')
+w3 = Web3(my_provider)
+# print(w3.eth.get_block('latest'))
+receipt = w3.eth.get_transaction_receipt(
+    "0xfed967fb2040275bbdcf9b0df0ced470018ef3f2506752be46a6d5b55cce5425")
+print(receipt.blockHash.hex())
+print(receipt.blockNumber)
+print(receipt.contractAddress)
+print(receipt)
+# for i in receipt.logs[1].topics:
+# 	print(i.hex()) 
+
