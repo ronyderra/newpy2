9524434f3b6a13c898a477868dfa6f9d91dfc602
rony derra <derrarony@gmail.com>
2023-01-24 21:16:55 +0200

    getTransactions

--- routes/user.py
+++ routes/user.py
@@ -3,14 +3,14 @@ from models.user import Transaction
 from config.db import conn
 from schemas.user import serializeDict, serializeList
 from bson import ObjectId
+import userTrxs
 user = APIRouter()
 
 @user.get('/')
 async def getTransactions():
     return serializeList(conn.MakerDao.transactions.find())
 
-
-@user.post('/')
-async def saveTransactions(transaction: Transaction):
-    conn.MakerDao.transactions.insert_one(dict(user))
-    return serializeList(conn.local.user.find())
+@user.post('/trxs')
+async def getUser():
+    trxs = userTrxs()
+    conn.MakerDao.transactions.insert_many(trxs)
