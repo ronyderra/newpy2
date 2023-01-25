eaf234772d569a46075a120231408e7d3f665eb4
rony derra <derrarony@gmail.com>
2023-01-25 20:59:54 +0200

    no need

--- schemas/user.py
+++ schemas/user.py
@@ -1,13 +1,3 @@
-def userEntity(item) -> dict:
-    return {
-        "id":str(item["_id"]),
-        "userAddress":item["userAddress"],
-        "trxs":item["trxs"],
-    }
-
-def usersEntity(entity) -> list:
-    return [userEntity(item) for item in entity]
-
 def serializeDict(a) -> dict:
     return {**{i:str(a[i]) for i in a if i=='_id'},**{i:a[i] for i in a if i!='_id'}}
 
